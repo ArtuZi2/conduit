f4c934e6410f2f10808746ea7c43c5ff8d683a9c
Mazurka Zita <tmazurkazita@gmail.com>
2021-07-07 14:01:21 +0200

    modified conduit tests

---
+++ test_seleniumside-tests/regisztracio.py
@@ -0,0 +1,19 @@
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+driver = webdriver.Chrome(ChromeDriverManager().install())
+
+driver.get("http://localhost:1667/#/")
+driver.find_element_by_xpath("/html/body//a[contains(@href,'register')]").click()
+driver.find_element_by_xpath("//input[@type='text'][@placeholder='Username']").send_keys("sun")
+driver.find_element_by_xpath("//input[@type='text'][@placeholder='Email']").send_keys("tkata@gmail.com")
+driver.find_element_by_xpath("//input[@type='password'][@placeholder='Password']").send_keys("Sunshine2046")
+driver.find_element_by_xpath("//button[@class='btn btn-lg btn-primary pull-xs-right']").click()
+
+"""except
+
+finally:
+pass
+
+#driver.close"""
