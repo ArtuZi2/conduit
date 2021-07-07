cf8ba4e3aa89ffb2612ab5cb1823c7d9365d9c50
Mazurka Zita <tmazurkazita@gmail.com>
2021-07-07 14:30:30 +0200

    modified regisztracio

--- test_seleniumside-tests/test_regisztracio.py
+++ test_seleniumside-tests/test_regisztracio.py
@@ -11,6 +11,8 @@ driver.find_element_by_xpath("//input[@type='text'][@placeholder='Email']").send
 driver.find_element_by_xpath("//input[@type='password'][@placeholder='Password']").send_keys("Sunshine2046")
 driver.find_element_by_xpath("//button[@class='btn btn-lg btn-primary pull-xs-right']").click()
 
+#assert megjelenik-e a home oldal, illetve a session, megjelenik-e a username
+
 """except
 
 finally:
