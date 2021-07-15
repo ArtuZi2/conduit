1b2e09fdc491523377f8dbcf73cc9ec35c7d7cf7
Mazurka Zita <tmazurkazita@gmail.com>
2021-07-15 14:13:15 +0200

    modified vizsgaremek

--- seleniumside-tests/001_register_test.py
+++ seleniumside-tests/001_register_test.py
@@ -32,28 +32,29 @@ def register(username, email, password):
 
     #driver.close"""
 
+try:
+    def register(username, email, password):
 
-def register(username, email, password):
-    driver.get("http://localhost:1667/#/")
-    driver.find_element_by_xpath("/html/body//a[contains(@href,'register')]").click()
-    driver.find_element_by_xpath("//input[@type='text'][@placeholder='Username']").send_keys(username)
-    driver.find_element_by_xpath("//input[@type='text'][@placeholder='Email']").send_keys(email)
+        driver.get("http://localhost:1667/#/")
+        driver.find_element_by_xpath("/html/body//a[contains(@href,'register')]").click()
+        driver.find_element_by_xpath("//input[@type='text'][@placeholder='Username']").send_keys(username)
+        driver.find_element_by_xpath("//input[@type='text'][@placeholder='Email']").send_keys(email)
     #if driver.find_element_by_class_name("swal-modal").is_displayed():
     #  a = driver.find_element_by_class_name("swal-button swal-button--confirm")
     #   a.click()
-    driver.find_element_by_xpath("//input[@type='password'][@placeholder='Password']").send_keys(password)
+        driver.find_element_by_xpath("//input[@type='password'][@placeholder='Password']").send_keys(password)
 
-    driver.find_element_by_xpath("//button[@class='btn btn-lg btn-primary pull-xs-right']").click()
+        driver.find_element_by_xpath("//button[@class='btn btn-lg btn-primary pull-xs-right']").click()
 
-    time.sleep(5)
+        time.sleep(5)
 
-    welcome = driver.find_element_by_xpath("//div[contains(@class, 'swal-text')]").text
-    text = "Your registration was successful!"
-    assert (text == welcome)
+        welcome = driver.find_element_by_xpath("//div[contains(@class, 'swal-text')]").text
+        text = "Your registration was successful!"
+        assert (text == welcome)
 
-    driver.find_element_by_xpath("//button[contains(text(),'OK')]").click()
+        driver.find_element_by_xpath("//button[contains(text(),'OK')]").click()
 
-    time.sleep(5)
+        time.sleep(5)
 
     #driver.find_element_by_xpath("//a[contains(@href, 'settings')]").click()
 
@@ -63,17 +64,22 @@ def register(username, email, password):
 
     #megjelen_username = driver.find_element_by_xpath("//a[contains(@href, '@sun2')]").text
 
-    megjelen_username = driver.find_element_by_xpath(f"//a[contains(@href, '@{username}')]").text
+        megjelen_username = driver.find_element_by_xpath(f"//a[contains(@href, '@{username}')]").text
     #driver.find_element_by_xpath("//fieldset[2]/input[@type='text']").send_keys("emma")
 
-    #print(megjelen_username)
+        print(megjelen_username)
     #print(username)
 
-    assert megjelen_username == username
+        assert megjelen_username == username
 
 
-register("sun2", "tkata24@gmail.com", "Sunshine2046")
-driver.close
+    register("sun2", "tkatat1122@gmail.com", "Sunshine2046")
+    driver.find_element_by_xpath("//ul/li[5]").click()
+
+finally:
+    driver.close()
+
+
 
 """try:
     driver.get("http://localhost:1667/#/")
---
+++ seleniumside-tests/003_logout_test.py
@@ -0,0 +1,26 @@
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+import time
+
+try:
+    driver = webdriver.Chrome(ChromeDriverManager().install())
+
+    driver.get("http://localhost:1667/#/")
+
+    navbar = driver.find_elements_by_class_name("ion-compose")
+    print(navbar)
+
+    element = driver.find_element_by_xpath("/html/body//a[contains(@href,'login')]")
+    print(element.is_enabled())
+    print(element.is_displayed())
+
+    element.click()
+    driver.find_element_by_xpath("//input[@type='text'][@placeholder='Email']").send_keys("tkata@gmail.com")
+    driver.find_element_by_xpath("//input[@type='password'][@placeholder='Password']").send_keys("Sunshine2046")
+    driver.find_element_by_xpath("//button[@class='btn btn-lg btn-primary pull-xs-right']").click()
+    time.sleep(2)
+    #logout = driver.find_element_by_class_name("ion-android-exit")
+    driver.find_element_by_xpath("//ul/li[5]").click()
+
+finally:
+    driver.close()
