a997ad13e43d5414e4d7cbbbfccfb66963d26e98
Mazurka Zita <tmazurkazita@gmail.com>
2021-07-26 18:45:55 +0200

    modified vizsgaremek

--- conduit_tests/004_new_post_tes.py
+++ conduit_tests/004_new_post_tes.py
@@ -4,24 +4,24 @@ import time
 import pytest
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
-"""options = Options()
+options = Options()
 options.add_argument('--headless')
 options.add_argument('--disable-gpu')
 import pytest
 
-driver = webdriver.Chrome(ChromeDriverManager().install())"""
+driver = webdriver.Chrome(ChromeDriverManager().install())
 
-@pytest.fixture(scope='session')
-def browser():
+"""@pytest.fixture(scope='session')
+def driver():
     driver = webdriver.Chrome()
     driver.get("http://localhost:1667/#/")
-    return driver
+    return driver"""
 
-def test_login(browser):
-    navbar = browser.find_elements_by_class_name("ion-compose")
+def test_login():
+    navbar = driver.find_elements_by_class_name("ion-compose")
     print(navbar)
 
-    element = browser.find_element_by_xpath("/html/body//a[contains(@href,'login')]")
+    element = driver.find_element_by_xpath("/html/body//a[contains(@href,'login')]")
     print(element.is_enabled())
     print(element.is_displayed())
     emil = "tkata@gmail.com"
@@ -29,40 +29,40 @@ def test_login(browser):
 
     element.click()
     time.sleep(2)
-    browser.find_element_by_xpath("//input[@type='text'][@placeholder='Email']").send_keys(emil)
-    browser.find_element_by_xpath("//input[@type='password'][@placeholder='Password']").send_keys(password)
-    browser.find_element_by_xpath("//button[@class='btn btn-lg btn-primary pull-xs-right']").click()
+    driver.find_element_by_xpath("//input[@type='text'][@placeholder='Email']").send_keys(emil)
+    driver.find_element_by_xpath("//input[@type='password'][@placeholder='Password']").send_keys(password)
+    driver.find_element_by_xpath("//button[@class='btn btn-lg btn-primary pull-xs-right']").click()
 
     time.sleep(2)
 
 #New Article button
-continue_link = browser.find_element_by_xpath("//a[@href='#/editor']").click()
+continue_link = driver.find_element_by_xpath("//a[@href='#/editor']").click()
 
-def test_new_post(browser):
+def test_new_post():
 
     time.sleep(2)
-    article = browser.find_element_by_xpath("//fieldset[1]/input")
+    article = driver.find_element_by_xpath("//fieldset[1]/input")
     article.send_keys("Spring")
     time.sleep(3)
-    browser.find_element_by_xpath("//fieldset[2]/input").send_keys("About my favorite season")
+    driver.find_element_by_xpath("//fieldset[2]/input").send_keys("About my favorite season")
     time.sleep(2)
-    browser.find_element_by_xpath("//fieldset[3]/textarea").send_keys("Spring is my life.")
+    driver.find_element_by_xpath("//fieldset[3]/textarea").send_keys("Spring is my life.")
     time.sleep(2)
-    browser.find_element_by_xpath("//fieldset[4]/div/div/ul/li/input").send_keys("Spring\n", "life\n", "season\n",
+    driver.find_element_by_xpath("//fieldset[4]/div/div/ul/li/input").send_keys("Spring\n", "life\n", "season\n",
                                                                                 "favorite\n")
     time.sleep(4)
-    browser.find_element_by_xpath("//button[@class='btn btn-lg pull-xs-right btn-primary']").click()
+    driver.find_element_by_xpath("//button[@class='btn btn-lg pull-xs-right btn-primary']").click()
 
 time.sleep(2)
 
-my_feed = browser.find_element_by_xpath("//a[@href='#/my-feed']")
+my_feed = driver.find_element_by_xpath("//a[@href='#/my-feed']")
 my_feed.click()
 
 
-def test_delete_post(browser):
-    delete_button = browser.find_element_by_xpath("//button[@class='btn btn-outline-danger btn-sm']")
+"""def test_delete_post():
+    delete_button = driver.find_element_by_xpath("//button[@class='btn btn-outline-danger btn-sm']")
     delete_button.click()
-    browser.find_elements_by_tag_name('button')[2].click()
+    driver.find_elements_by_tag_name('button')[2].click()"""
 
 
 #assert home és my article oldalon, hogy a cikk látható-e, kattintható-e, elérhető-e
--- conduit_tests/007_save_tes.py
+++ conduit_tests/007_save_tes.py
@@ -1,15 +1,21 @@
 from selenium import webdriver
+from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 import time
 from selenium.webdriver.chrome.options import Options
 import random, string
-options = Options()
-options.add_argument('--headless')
-options.add_argument('--disable-gpu')
+from selenium.webdriver.chrome.options import Options
 
-driver = webdriver.Chrome(ChromeDriverManager().install())
+options = webdriver.ChromeOptions()
+options.add_argument('--headless')
+driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
+driver.get("http://localhost:1667")
+
 
 def test_save():
 
     driver.get("http://localhost:1667/#/")
+
+
 test_save()
+
