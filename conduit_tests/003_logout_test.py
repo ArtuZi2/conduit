e15626f9b3205b7529a2efca937b1bde8b7cc6f6
Mazurka Zita <tmazurkazita@gmail.com>
2021-07-27 15:49:22 +0200

    modified vizsgaremek

--- conduit_tests/003_logout_test.py
+++ conduit_tests/003_logout_test.py
@@ -3,8 +3,8 @@ from webdriver_manager.chrome import ChromeDriverManager
 import time
 from selenium.webdriver.chrome.options import Options
 options = Options()
-#options.add_argument('--headless')
-#options.add_argument('--disable-gpu')
+options.add_argument('--headless')
+options.add_argument('--disable-gpu')
 
 driver = webdriver.Chrome(ChromeDriverManager().install())
 
