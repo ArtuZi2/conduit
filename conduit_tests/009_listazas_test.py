5427a08626af32f24867b2397c35c0beaa9eba8b
Mazurka Zita <tmazurkazita@gmail.com>
2021-07-28 13:43:35 +0200

    modified vizsgaremek

--- conduit_tests/009_listazas_test.py
+++ conduit_tests/009_listazas_test.py
@@ -4,8 +4,8 @@ import time
 import pytest
 from selenium.webdriver.chrome.options import Options
 options = Options()
-#options.add_argument('--headless')
-#options.add_argument('--disable-gpu')
+options.add_argument('--headless')
+options.add_argument('--disable-gpu')
 
 driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
 
