ced1d9368918751982bce341afa758b38923fc2a
Mazurka Zita <tmazurkazita@gmail.com>
2021-08-15 21:57:52 +0200

    modified vizsgaremek

--- conduit_tests/login_02_test.py
+++ conduit_tests/login_02_test.py
@@ -4,8 +4,8 @@ import time
 import random, string
 from selenium.webdriver.chrome.options import Options
 options = webdriver.ChromeOptions()
-#options.add_argument('--headless')
-#options.add_argument('--disable-gpu')
+options.add_argument('--headless')
+options.add_argument('--disable-gpu')
 driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
 
 
